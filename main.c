b474cc06b887bf5024958c210379e4f00b862df5
derrickkarake <47511082+derrickkarake@users.noreply.github.com>
2019-12-12 23:31:23 -0600

    Update main.c

--- main.c
+++ main.c
@@ -9,31 +9,33 @@ double getRand(unsigned int *seed) {
 }
 
 long double Calculate_Pi_Sequential(long long number_of_tosses) {
-    long long number_in_circle = 0;
+    long long number_in_circle = 0;              
     unsigned int seed = (unsigned int) time(NULL);
+    // generates random x,y cordinates for every toss
     for (long long int toss = 0; toss < number_of_tosses; toss++) {
-        double x = getRand(&seed);
-        double y = getRand(&seed);
+        double x = getRand(&seed);                                   // random x gerator
+        double y = getRand(&seed);                                   // radom y generator
         double distance_squared = x*x + y*y;
         if (distance_squared <= 1) number_in_circle++;
     }
     return 4 * number_in_circle/((double) number_of_tosses);
 }
 
-long double Calculate_Pi_Parallel(long long number_of_tosses) {
-    long long number_in_circle = 0;
-#pragma omp parallel num_threads(20)
+long double Calculate_Pi_Parallel(long long number_of_tosses) {             //holds the number of tosses 
+    long long number_in_circle = 0;                                         //holds the number of point in the cirlce
+#pragma omp parallel num_threads(20) //sets number of threads to 20
 {
     unsigned int seed = (unsigned int) time(NULL) + (unsigned int) omp_get_thread_num();
-    #pragma omp for reduction(+: number_in_circle)
+    #pragma omp for reduction(+: number_in_circle)              //summing with the reduction clause and saving it number_in_cicle
+     // generates random x,y cordinates for every toss
         for (long long int toss = 0; toss < number_of_tosses; toss++) {
-            double x = getRand(&seed);
+            double x = getRand(&seed);                            //generates random points
             double y = getRand(&seed);
             double distance_squared = x*x + y*y;
-            if (distance_squared <= 1) number_in_circle++;
+            if (distance_squared <= 1) number_in_circle++;         //increameants numbers_in_cicle when disance squared is less than 1(when in the cicle
         }
     }
-    return 4 * number_in_circle/((double) number_of_tosses);
+    return 4 * number_in_circle/((double) number_of_tosses);     //returns an estimated value of pi using 
 }
 
 int main() {
